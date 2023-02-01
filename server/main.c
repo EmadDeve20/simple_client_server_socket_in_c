946ca7513efcda54452bce170407d011b08d0db8
EmadDeve20 <emaddeve20@gmail.com>
2023-02-01 22:42:40 +0330

    try to fix server

--- server/main.c
+++ server/main.c
@@ -4,77 +4,70 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <stdlib.h>
 
 #define SERVER_MESSAGE_SIZE 1024
 #define CLIENT_MESSAGE_SIZE 500
-
-int create_socket()
-{
-    int sock;
-    puts("Create the socket\n");
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    return sock;
-}
-
-int bind_socket(int sock)
-{
-    int iRetval=-1;
-    int ClientPort = 8888;
-    struct sockaddr_in  remote= {0};
-    // Internet address family
-    remote.sin_family = AF_INET;
-    // Any incoming interface
-    remote.sin_addr.s_addr = htonl(INADDR_ANY);
-    iRetval = bind(sock,(struct sockaddr *)&remote,sizeof(remote));
-    return iRetval;
-}
+#define PORT 8888
 
 int main(int argc, char *argv[])
 {
-    int socket_desc, sock, clientLen, read_size;
+    int master_socket, new_socket, clientLen, read_size, opt = 1;
     struct sockaddr_in client_address;
     char message[SERVER_MESSAGE_SIZE] = {0};
     char client_message[CLIENT_MESSAGE_SIZE];
     const char *pMessage = "hello aticleworld.com";
-    //Create socket
-    socket_desc = create_socket();
-    if (socket_desc == -1)
+   
+    //init master socket
+    if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("Could not create socket");
-        return 1;
+        perror("Socket Initialization Failed!");
+        exit(EXIT_FAILURE);
     }
-    //Bind
-    if( bind_socket(socket_desc) < 0)
+
+    // attaching socket to the port
+    if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
     {
-        //print the error message
-        perror("bind failed.");
-        return 1;
+        perror("attaching port for the socket failed!");
+        exit(EXIT_FAILURE);
     }
-    //Listen
-    if (listen(socket_desc, 3) == -1)
+
+    // the type of client IP address is IPv4  (AF_INET)
+    client_address.sin_family = AF_INET;
+    client_address.sin_addr.s_addr = INADDR_ANY;
+    client_address.sin_port = htons(PORT);
+
+    if (bind(master_socket, (struct sockaddr*)&client_address, sizeof(client_address)) < 0)
+    {
+        perror("The binding Failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    //Listen: 3 connection request will be queued
+    if (listen(master_socket, 3) == -1)
     {
         perror("listen failed.");
         return 1;
     }
-    //Accept and incoming connection
+
+    printf("Waiting for incoming connections...\n");
+
     while(1)
     {
-        printf("Waiting for incoming connections...\n");
-        clientLen = sizeof(struct sockaddr_in);
         //accept connection from an incoming client
-        sock = accept(socket_desc,(struct sockaddr *)&client_address,(socklen_t*)&clientLen);
-        if (sock < 0)
+        if ((new_socket = accept(master_socket,(struct sockaddr *)&client_address,(socklen_t*)&clientLen)) < 0)
         {
             perror("accept failed");
             return 1;
         }
+
         printf("Connection accepted\nClient Ip: %s\tClient Port: %d\n", inet_ntoa(client_address.sin_addr),
         ntohs(client_address.sin_port));
         memset(client_message, '\0', sizeof client_message);
         memset(message, '\0', sizeof message);
 
         //Receive a reply from the client
-        if( recv(sock, client_message, 500, 0) < 0)
+        if( recv(new_socket, client_message, 500, 0) < 0)
         {
             printf("recv failed");
             break;
@@ -84,14 +77,16 @@ int main(int argc, char *argv[])
         
         snprintf(message,  strlen(client_message), "Server saw your message: %s", client_message);
 
-        if( send(sock, message, strlen(message), 0) < 0)
+        if( send(new_socket, message, strlen(message), 0) < 0)
         {
             printf("Send failed");
             return 1;
         }
 
-        close(sock);
-        sleep(1);
+        close(new_socket);
     }
+
+    shutdown(master_socket, SHUT_RDWR);
+    
     return 0;
 }
